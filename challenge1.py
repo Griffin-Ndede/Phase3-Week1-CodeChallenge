8525292ae8345b84917d96cb97e25051c7604ef0
Griffin-Ndede <griffin.omondi@students.moringaschool.com>
2023-08-22 23:27:55 +0300

    Completed challenge 1

---
+++ challenge1.py
@@ -0,0 +1,15 @@
+#  to convert pm to 24hr add 12 to the hour if it is between 1 and 11
+# to convert am to 24hr change 12 to 0 and leave it as it is if hour is between 1 and 11
+
+def hour_converter(hour, minute, period):
+    if period == "am" and hour == 12:
+        hour = 0
+    elif period == "pm" and hour != 12:
+        hour += 12
+
+    # format the hour and minutes to strings
+    hour_str = str(hour).zfill(2)
+    minute_str = str(minute).zfill(2)
+
+    return hour_str + minute_str
+
